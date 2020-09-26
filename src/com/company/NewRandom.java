5452e4c3850a24e3bbb190a44ec52296a601a417
lreubenstein <lreubenstein@billericak12.com>
2020-09-26 10:20:27 -0400

    sds

--- src/com/company/NewRandom.java
+++ src/com/company/NewRandom.java
@@ -2,12 +2,9 @@ package com.company;
 
 public class NewRandom {
 
-	public NewRandom() {
-		// TODO Auto-generated constructor stub
-	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		//double r = Math.random();  //[0.0,1.0)
 		//double r = Math.random()*10; //[0.0, 10.0)
 		//int r = (int)(Math.random()*10);  //[0,9]
